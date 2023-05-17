41316c306da947405e31788b5480c6880d5554ce
duongtranchautrung <duong.tranchautrung@gmail.com>
2023-05-17 21:20:38 +0700

    video3

---
+++ video3/video3.js
@@ -0,0 +1,10 @@
+console.log('Hello world video3')
+
+let c = 5;
+let d = 6.9;
+
+//dau `` sẽ k phân biệt bên trong nó là '' hay ""
+
+console.log('Hello world video3',d,c)
+
+console.log('Hello world video3',"x=", c, "y=",d)
